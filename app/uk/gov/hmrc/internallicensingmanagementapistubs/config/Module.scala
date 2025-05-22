33c45eb1390836bb07b42b66a88f190e6d136d73
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2025-05-22 15:36:49 +0000

    'Creating new service internal-licensing-management-api-stubs'

---
+++ app/uk/gov/hmrc/internallicensingmanagementapistubs/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.internallicensingmanagementapistubs.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
